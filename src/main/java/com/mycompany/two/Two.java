031e372233c82db216b2d7a7dc90e71d875f4393
thaksaum <thaksaum@sheridancollege.ca>
2023-02-20 16:28:44 -0500

    lets go

---
+++ src/main/java/com/mycompany/two/Two.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.two;
+
+/**
+ *
+ * @author saumy
+ */
+public class Two {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
